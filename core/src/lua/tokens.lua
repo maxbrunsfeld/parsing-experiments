eb066acc4899e61be09b0bf20a3ef31c29316e30
Max Brunsfeld <pair+mbrunsfeld@pivotallabs.com>
2013-10-04 13:25:29 -0700

    Move regex compilation code into tokens file

--- core/src/lua/tokens.lua
+++ core/src/lua/tokens.lua
@@ -1,20 +1,111 @@
-local regex = require("regex")
 local Rules = require("rules")
 local Struct = require("struct")
 
+local CHAR_CLASS_MAP = {
+  d = { "digit", true },
+  D = { "digit", false },
+  s = { "space", true },
+  S = { "space", false },
+  w = { "word", true },
+  W = { "word", false }
+}
+
+local RegexParser = Struct({ 'input' }, {
+  initialize = function(self, input)
+    self.input = input
+    self.i = 1
+    self.len = string.len(input)
+  end,
+
+  main = function(self)
+    local term = self:term()
+    if self:has_more() and self:peek() == '|' then
+      self:consume(1)
+      return Rules.Choice(term, self:term())
+    else
+      return term
+    end
+  end,
+
+  term = function(self)
+    local result = Rules.End
+    while self:has_more() and (self:peek() ~= '|') do
+      result = Rules.Seq(result, self:factor())
+    end
+    return result
+  end,
+
+  factor = function(self)
+    local result = self:atom()
+    if self:has_more() then
+      local char = self:peek()
+      if char == '*' then
+        self:consume(1)
+        result = Rules.Repeat(result)
+      elseif char == '+' then
+        self:consume(1)
+        result = Rules.Seq(result, Rules.Repeat(result))
+      end
+    end
+    return result
+  end,
+
+  atom = function(self)
+    if self:peek() == "\\" then
+      self:consume(1)
+      local char = self:next()
+      local class_desc = CHAR_CLASS_MAP[char]
+      if not class_desc then
+        error("Unknown character class: '\\" .. char .."'")
+      end
+      return Rules.CharClass[class_desc[1]](class_desc[2])
+    else
+      return Rules.Char(self:next())
+    end
+  end,
+
+  -- primitives
+
+  next = function(self)
+    local result = self:peek()
+    self:consume(1)
+    return result
+  end,
+
+  peek = function(self)
+    return string.sub(self.input, self.i, self.i)
+  end,
+
+  consume = function(self, n)
+    self.i = self.i + n
+  end,
+
+  has_more = function(self)
+    return self.i <= self.len
+  end
+})
+
 local String = Struct({ 'value' }, {
-  transitions = function(self)
+  to_rule = function(self)
     local result = Rules.End
     for i = 1, string.len(self.value) do
       result = Rules.Seq(result, Rules.Char(string.sub(self.value, i, i)))
     end
-    return result:transitions()
+    return result
+  end,
+
+  transitions = function(self)
+    return self:to_rule():transitions()
   end
 })
 
 local Pattern = Struct({ 'value' }, {
+  to_rule = function(self)
+    return RegexParser(self.value):main()
+  end,
+
   transitions = function(self)
-    return regex(self.value):transitions()
+    return self:to_rule():transitions()
   end
 })
 
--- core/test/lua/tokens_spec.lua
+++ core/test/lua/tokens_spec.lua
@@ -6,6 +6,92 @@ local Rules = require("rules")
 describe("Tokens", function()
   local token
 
+  describe("converting tokens to rule trees", function()
+    local rule
+
+    describe("regex tokens", function()
+      local function to_rule(string)
+        return Tokens.Pattern(string):to_rule()
+      end
+
+      it("parses simple strings", function()
+        rule = to_rule("lol")
+
+        assert.are.same(
+          _seq(_seq(_char("l"), _char("o")), _char("l")),
+          rule)
+      end)
+
+      it("parses choices", function()
+        rule = to_rule("lol|hah")
+
+        assert.are.same(
+          _choice(
+            _seq(_seq(_char("l"), _char("o")), _char("l")),
+            _seq(_seq(_char("h"), _char("a")), _char("h"))),
+          rule)
+      end)
+
+      it("parses character classes", function()
+        rule = to_rule("\\d")
+        assert.are.same(_class.digit(true), rule)
+
+        rule = to_rule("\\s")
+        assert.are.same(_class.space(true), rule)
+
+        rule = to_rule("\\w")
+        assert.are.same(_class.word(true), rule)
+
+        rule = to_rule("\\S")
+        assert.are.same(_class.space(false), rule)
+
+        rule = to_rule("\\W")
+        assert.are.same(_class.word(false), rule)
+      end)
+
+      it("raises an error for unknown character classes", function()
+        assert.has_error(function()
+          to_rule("\\z")
+        end, "Unknown character class: '\\z'")
+      end)
+
+      it("parses repetitions", function()
+        rule = to_rule("\\s*")
+        assert.are.same(_rep(_class.space(true)), rule)
+      end)
+
+      it("parses one-or-more repetitions", function()
+        rule = to_rule("x+")
+        assert.are.same(_seq(_char("x"), _rep(_char("x"))), rule)
+      end)
+    end)
+
+    describe("string tokens", function()
+      local function to_rule(string)
+        return Tokens.String(string):to_rule()
+      end
+
+      it("returns a sequence of characters", function()
+        rule = to_rule("abcd")
+
+        assert.are.same(
+          _seq(
+            _seq(
+              _seq(
+                _char("a"),
+                _char("b")),
+              _char("c")),
+            _char("d")),
+          rule)
+      end)
+
+      it("handles single characters", function()
+        rule = to_rule("a")
+        assert.are.same(_char("a"), rule)
+      end)
+    end)
+  end)
+
   describe("calculating transitions", function()
     describe("regex patterns", function()
       it("compiles the regex to a tree of rules", function()
@@ -18,7 +104,7 @@ describe("Tokens", function()
 
     describe("strings", function()
       it("reduces to a sequence of characters", function()
-        token = Tokens.Pattern("abcd")
+        token = Tokens.String("abcd")
         assert.are.same(
           token:transitions(),
           _seq(
