a9e494f2c2cc0d35225427a8973b1252150940d8
Max Brunsfeld <pair+mbrunsfeld@pivotallabs.com>
2013-10-13 18:29:15 -0700

    Add code generation for state transitions and actions

--- core/src/lua/code_generator.lua
+++ core/src/lua/code_generator.lua
@@ -1,6 +1,7 @@
 local Struct = require("Struct")
 local util = require("util")
 local Rules = require("rules")
+local StateMachine = require("state_machine")
 
 -- string utils
 local function join(strings, sep)
@@ -14,12 +15,24 @@ local function join(strings, sep)
 end
 
 local SHIFT_WIDTH = 4
+local LOOKAHEAD_CHAR = "lookahead_char"
+local LOOKAHEAD_SYM = "lookahead_sym"
+
+local CHAR_CLASS_FNS = {
+  space = "iswhitespace",
+  digit = "isdigit",
+  word = "isalnum",
+}
 
 return Struct({ "state_machine", "grammar_name" }, {
   initialize = function(self, state_machine, grammar_name)
     self.state_machine = state_machine
     self.grammar_name = grammar_name
     self.indent = 0
+
+    for i, state in ipairs(state_machine.states) do
+      state.index = i
+    end
   end,
 
   code = function(self)
@@ -39,8 +52,8 @@ return Struct({ "state_machine", "grammar_name" }, {
       {"const char ** input_string"},
       function()
         return join(
-          util.map(self.state_machine.states, function(state, i)
-            return self:labeled("state_" .. i, function()
+          util.map(self.state_machine.states, function(state)
+            return self:labeled(self:label_for_state(state), function()
               return self:code_for_state(state)
             end)
           end), "\n")
@@ -48,22 +61,70 @@ return Struct({ "state_machine", "grammar_name" }, {
   end,
 
   code_for_state = function(self, state, i)
-    return join(util.map(state.transitions, function(transition)
+    if state.action then
+      return self:code_for_state_action(state.action)
+    else
+      return self:code_for_state_transitions(state.transitions)
+    end
+  end,
+
+  code_for_state_action = function(self, action)
+    if action == StateMachine.Actions.Accept then
+      return self:statements({
+        self:fn_call("accept")
+      })
+    elseif action.class == StateMachine.Actions.Reduce then
+      return self:statements({
+        self:fn_call("reduce", { action.symbol_count, self:sym(action.new_sym) })
+      })
+    else
+      error("Unknown action: " .. P.dump(action))
+    end
+  end,
+
+  code_for_state_transitions = function(self, transitions)
+    return join(util.map(transitions, function(transition)
       return self:_if(self:code_for_transition_on(transition[1]), function()
-        return "stuff"
+        return self:_goto(self:label_for_state(transition[2]))
       end)
     end), "\n")
   end,
 
+  label_for_state = function(self, state, i)
+    return "state_" .. state.index
+  end,
+
   code_for_transition_on = function(self, transition_on)
     if (transition_on.class == Rules.Char) then
-      return "next_char == " .. self:char(transition_on.value)
+      return LOOKAHEAD_CHAR .. " == " .. self:char(transition_on.value)
+    elseif (transition_on.class == Rules.CharClass) then
+      return self:fn_call(self:fn_for_char_class(transition_on.name), { LOOKAHEAD_CHAR })
+    elseif (transition_on.class == Rules.Sym) then
+      return LOOKAHEAD_SYM .. " == " .. self:sym(transition_on.name)
     else
-      return "true"
+      error("Unknown transition type: " .. P.write(transition_on))
     end
   end,
 
+  fn_for_char_class = function(self, class_name)
+    return CHAR_CLASS_FNS[class_name] or
+      error("Unknown character class: " .. class_name)
+  end,
+
+  sym = function(self, name)
+    return "SYM_" .. name
+  end,
+
   -- generic C code helpers
+
+  fn_call = function(self, fn_name, args)
+    local args_string = args and join(args, ", ") or ""
+    return fn_name .. "(" .. args_string .. ")"
+  end,
+
+  _goto = function(self, label_name)
+    return self:statements({ "goto " .. label_name })
+  end,
 
   char = function(self, value)
     return "'" .. value .. "'"
@@ -75,8 +136,7 @@ return Struct({ "state_machine", "grammar_name" }, {
 
   _if = function(self, condition, body_fn)
     return self:lines({
-      "if (" .. condition .. ")",
-      "{",
+      "if (" .. condition .. ") {",
       self:indented(body_fn),
       "}"
     })
@@ -97,6 +157,12 @@ return Struct({ "state_machine", "grammar_name" }, {
     local result = fn()
     self.indent = self.indent - 1
     return result
+  end,
+
+  statements = function(self, inputs)
+    return self:lines(util.map(inputs, function(input)
+      return input .. ";"
+    end))
   end,
 
   lines = function(self, inputs)
--- core/test/lua/code_generator_spec.lua
+++ core/test/lua/code_generator_spec.lua
@@ -1,5 +1,6 @@
 local StateMachine = require("state_machine")
 local CodeGenerator = require("code_generator")
+local Rules = require("rules")
 
 describe("CodeGenerator", function()
   local generator
@@ -7,8 +8,13 @@ describe("CodeGenerator", function()
   before_each(function()
     state_machine = StateMachine({})
       :add_state(1)
-      :add_state(2)
-      :add_transition(1, "hi", 2)
+      :add_state(2, StateMachine.Actions.Reduce(1, "rule1"))
+      :add_state(3, StateMachine.Actions.Reduce(1, "rule2"))
+      :add_state(4, StateMachine.Actions.Accept)
+      :add_transition(1, Rules.Char("a"), 2)
+      :add_transition(1, Rules.CharClass:digit(true), 3)
+      :add_transition(1, Rules.Sym("rule1"), 4)
+
     generator = CodeGenerator(state_machine, "the_grammar")
   end)
 
